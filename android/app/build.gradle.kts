e014bb6741c205b4b68958bad3c23666c2f6a189
marimoisreal <lolkeklol626@gmail.com>
2026-01-20 13:03:05 +0100

    fix: forced versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,32 +7,28 @@ plugins {
 
 android {
     namespace = "com.example.spot_saver"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.spot_saver"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.spot_saver"
-        minSdk = 21 
-        targetSdk = 35 
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        jvmTarget = "1.8"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
@@ -41,4 +37,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,10 +6,11 @@ allprojects {
     }
 }
 
-extra["compileSdkVersion"] = 34
-extra["compileSdk"] = 34
-extra["targetSdkVersion"] = 34
-extra["targetSdk"] = 34
+// Actual versions as required(2026)
+extra["compileSdkVersion"] = 35
+extra["compileSdk"] = 35
+extra["targetSdkVersion"] = 35
+extra["targetSdk"] = 35
 extra["minSdkVersion"] = 21
 extra["minSdk"] = 21
 
@@ -22,6 +23,15 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    // Force SDK 35
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileSdkVersion(35)
+        }
+    }
+    
     evaluationDependsOn(":app")
 }
 
